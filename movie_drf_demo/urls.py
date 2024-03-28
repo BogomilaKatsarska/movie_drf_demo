03ea2a7c54ed21524b968e8de299a5e26dcec5b3
Bogomila <bkatsarska@abv.bg>
2024-03-28 09:09:40 +0200

    added pagination, search functionality

--- movie_drf_demo/urls.py
+++ movie_drf_demo/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
+from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet, movie_list
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('action/', ActionViewSet.as_view({'get': 'list'}), name='action-movies'),
     path('comedy/', ComedyViewSet.as_view({'get': 'list'}), name='comedy-movies'),
+    path('all-movies/', movie_list, name='all-movies'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/views.py
+++ movies/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from movies.serializers import MovieSerializer
 from movies.models import MovieData
+from django.core.paginator import Paginator
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = MovieData.objects.all()
     serializer_class = MovieSerializer
+
+
+def movie_list(request):
+    movie_objs = MovieData.objects.all()
+
+    movie_name = request.GET.get('movie_name')
+
+    if movie_name != '' and movie_name is not None:
+        movie_objs = movie_objs.filter(name__icontains=movie_name)
+
+    paginator = Paginator(movie_objs, 4)
+    page = request.GET.get('page')
+    movie_objs = paginator.get_page(page)
+    context = {
+        'movie_objs': movie_objs,
+    }
+    return render(request, 'movies/movie_list.html', context)
 
 
 class ActionViewSet(viewsets.ModelViewSet):
